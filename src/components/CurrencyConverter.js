31b23fcc50d4f94f805fa5f992e0b26ba81b088e
annascaria3010 <annascaria01@gmail.com>
2024-12-11 15:19:54 +0900

    currency converter swap icon

--- src/components/CurrencyConverter.js
+++ src/components/CurrencyConverter.js
@@ -6,6 +6,11 @@ const CurrencyConverter = () => {
     const [fromCurrency, setFromCurrency] = useState("JPY");
     const [toCurrency, setToCurrency] = useState("INR");
     
+    const handleSwapCurrencies =() => {
+        setFromCurrency(toCurrency);
+        setToCurrency(fromCurrency);
+    }
+
     return (
     <div className='currency-converter'>
        <h2 className='converter-title'>Currency Converter</h2>
@@ -24,7 +29,7 @@ const CurrencyConverter = () => {
                     />
                 </div>
 
-                <div className='swap-icon'>
+                <div className='swap-icon' onClick={handleSwapCurrencies}>
                 <svg width="16" viewBox="0 0 20 19" xmlns="http://www.w3.org/2000/svg">
                     <path 
                         d="M19.13 11.66H.22a.22.22 0 0 0-.22.22v1.62a.22.22 0 0 0 .22.22h16.45l-3.92 4.94a.22.22 0 0 0 .17.35h1.97c.13 0 .25-.06.33-.16l4.59-5.78a.9.9 0 0 0-.7-1.43zM19.78 5.29H3.34L7.26.35A.22.22 0 0 0 7.09 0H5.12a.22.22 0 0 0-.34.16L.19 5.94a.9.9 0 0 0 .68 1.4H19.78a.22.22 0 0 0 .22-.22V5.51a.22.22 0 0 0-.22-.22z"
